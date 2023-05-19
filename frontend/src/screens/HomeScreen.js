2dc798a602fd36d46d6f3801c3832ddc8b2e2118
tyuukau <bachocnhi2002@gmail.com>
2023-05-19 12:42:26 +0700

    Redux initialised

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,34 +1,48 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 
 import { Row, Col } from "react-bootstrap";
 
+import { useDispatch, useSelector } from "react-redux";
+
 // import products from "../products";
 import Product from "../components/Product";
+import { listProducts } from "../actions/productActions";
 
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { error, loading, products } = productList;
+
+  // const [products, setProducts] = useState([]);
 
   useEffect(() => {
     console.log("Use Effect triggered.");
-    async function fetchProducts() {
-      const { data } = await axios.get("/api/products/");
-      setProducts(data);
-    }
-    fetchProducts();
-  }, []);
+    // async function fetchProducts() {
+    //   const { data } = await axios.get("/api/products/");
+    //   setProducts(data);
+    // }
+    // fetchProducts();
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <div>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            {/* <h3>{product.name}</h3> */}
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h2> {error} </h2>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              {/* <h3>{product.name}</h3> */}
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </div>
   );
 }
---
+++ frontend/src/store.js
@@ -0,0 +1,35 @@
+// file: store.ts
+import { configureStore } from '@reduxjs/toolkit'
+
+// We'll use redux-logger just as an example of adding another middleware
+import logger from 'redux-logger'
+
+// And use redux-batched-subscribe as an example of adding enhancers
+// import { batchedSubscribe } from 'redux-batched-subscribe'
+
+import { productListReducer } from './reducers/productReducers'
+
+const reducer = {
+    productList: productListReducer,
+}
+
+const preloadedState = {
+}
+
+// const debounceNotify = _.debounce((notify) => notify())
+
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  devTools: process.env.NODE_ENV !== 'production',
+  preloadedState,
+//   enhancers: [batchedSubscribe(debounceNotify)],
+})
+
+// The store has been created with these options:
+// - The slice reducers were automatically passed to combineReducers()
+// - redux-thunk and redux-logger were added as middleware
+// - The Redux DevTools Extension is disabled for production
+// - The middleware, batched subscribe, and devtools enhancers were composed together
+
+export default store
