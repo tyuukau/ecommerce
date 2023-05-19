310f3164a79b4847d2668f091ae83268bbc2507e
tyuukau <bachocnhi2002@gmail.com>
2023-05-19 16:29:10 +0700

    Display Quantity to Buy in ProductScreen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 // import axios from "axios";
 
 import { Link, useParams } from "react-router-dom";
-import { Row, Col, Button, Image, ListGroup, Card } from "react-bootstrap";
+import { Row, Col, Button, Image, ListGroup, Card, Form } from "react-bootstrap";
 import Rating from "../components/Rating";
 
 // import Product from "../components/Product";
@@ -14,6 +14,8 @@ import Loader from "../components/Loader";
 import Message from "../components/Message";
 
 function ProductScreen() {
+  const [qty, setQty] = useState(1);
+
   const dispatch = useDispatch();
   const productDetails = useSelector((state) => state.productDetails);
   const { error, loading, product } = productDetails;
@@ -85,6 +87,27 @@ function ProductScreen() {
                     </Col>
                   </Row>
                 </ListGroup.Item>
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty
+                      </Col>
+                      <Col xs='auto' className='my-1'>
+                      <Form.Control 
+                      as="select"
+                      value ={qty}
+                      onChange={(e) => setQty(e.target.value)}>
+                        {
+                          [...Array(product.countInStock).keys()].map((x) => 
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>)
+                        }
+                      </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
                 <ListGroup.Item>
                   <Button
                     class="button-block"
