4364e64f48b02efa3d401bfe08a7caa424c39d7a
tyuukau <bachocnhi2002@gmail.com>
2023-05-21 08:32:27 +0700

    Backend: new updateUserProfile() function

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -45,7 +45,7 @@ function RegisterScreen() {
   const submitHandler = (e) => {
     e.preventDefault();
 
-    if (password != confirmPassword) {
+    if (password !== confirmPassword) {
       setMessage("Passwords do not match");
     } else {
       dispatch(register(name, email, password));
