8239b829ca37d3cd733f6fec9483e5362c466e7d
tyuukau <bachocnhi2002@gmail.com>
2023-05-21 08:34:52 +0700

    Backend: Register updateUserProfile() view
    Path 'profile/update/'

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.getUsers, name='users'),
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('profile/', views.getUserProfile, name='user_profile'),
+    path('profile/update/', views.updateUserProfile, name='user_update_profile'),
     path('register/', views.createUser, name='register'),
 ]
