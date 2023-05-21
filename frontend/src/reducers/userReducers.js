8935a3130526425f1c6a11ca19621ffa58124521
tyuukau <bachocnhi2002@gmail.com>
2023-05-21 18:13:26 +0700

    Frontend: Adding userProfileReducer
    And register it with the store

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -6,6 +6,9 @@ import {
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
   USER_REGISTER_FAIL,
+  USER_PROFILE_REQUEST,
+  USER_PROFILE_SUCCESS,
+  USER_PROFILE_FAIL,
 } from "../constants/userConstants";
 
 /**
@@ -55,6 +58,19 @@ export const userRegisterReducer = (state = {}, action) => {
       return { loading: false, error: action.payload };
     case USER_LOGOUT:
       return {};
+    default:
+      return state;
+  }
+};
+
+export const userProfileReducer = (state = { user: {} }, action) => {
+  switch (action.type) {
+    case USER_PROFILE_REQUEST:
+      return { loading: true, ...state };
+    case USER_PROFILE_SUCCESS:
+      return { loading: false, userInfo: action.payload };
+    case USER_PROFILE_FAIL:
+      return { loading: false, error: action.payload };
     default:
       return state;
   }
