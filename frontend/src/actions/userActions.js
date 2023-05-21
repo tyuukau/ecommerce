87181866d5f7379c964a91fb7207bb5a358b644a
tyuukau <bachocnhi2002@gmail.com>
2023-05-21 07:56:02 +0700

    Register user: Adding register action
    API:
    POST /api/users/register

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -5,6 +5,9 @@ import {
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
 } from "../constants/userConstants";
 
 /**
@@ -27,7 +30,7 @@ export const login = (email, password) => async (dispatch) => {
 
     const { data } = await axios.post(
       "/api/users/login/",
-      { username: email, password: password },
+      { 'username': email, 'password': password },
       config
     );
 
@@ -51,4 +54,37 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
   localStorage.removeItem("userInfo");
   dispatch({ type: USER_LOGOUT });
+};
+
+export const register = (name, email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: USER_REGISTER_REQUEST });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post(
+      "/api/users/register/",
+      { 'name': name, 'username': email, 'password': password },
+      config
+    );
+
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data,
+    });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
