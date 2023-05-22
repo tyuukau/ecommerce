d025ff01236431c05a086979c9e25c4b1d8357c7
tyuukau <bachocnhi2002@gmail.com>
2023-05-22 09:45:14 +0700

    Implement userProfileUpdate in ProfileScreen

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -10,7 +10,9 @@ import Loader from "../components/Loader";
 import Message from "../components/Message";
 import FormContainer from "../components/FormContainer";
 
-import { getUserProfile } from "../actions/userActions";
+import { getUserProfile, updateUserProfile } from "../actions/userActions";
+
+import { USER_PROFILE_UPDATE_RESET } from "../constants/userConstants";
 
 function ProfileScreen() {
   const [email, setEmail] = useState("");
@@ -28,20 +30,24 @@ function ProfileScreen() {
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
+  const userProfileUpdate = useSelector((state) => state.userProfileUpdate);
+  const { success } = userProfileUpdate;
+
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!userInfo) {
       navigate("/login");
     } else {
-      if (!userProfile || !userProfile.name) {
+      if (!userProfile || !userProfile.name || success) {
+        dispatch({type: USER_PROFILE_UPDATE_RESET })
         dispatch(getUserProfile("profile"));
       } else {
         setName(userProfile.name);
         setEmail(userProfile.email);
       }
     }
-  }, [dispatch, navigate, userInfo, userProfile]);
+  }, [dispatch, navigate, userInfo, userProfile, success]);
 
   /**
    * This function prevents the default form submission behavior and dispatches a login action with the
@@ -56,8 +62,16 @@ function ProfileScreen() {
     if (password !== confirmPassword) {
       setMessage("Passwords do not match");
     } else {
-      console.log("Updating...");
-    } 
+      // console.log("Updating...");
+      dispatch(
+        updateUserProfile({
+          id: userDetails._id,
+          name: name,
+          email: email,
+          password: password,
+        })
+      );
+    }
   };
 
   return (
