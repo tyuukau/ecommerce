0b3737771150f89d9fe8f2259f16374c6aa9d8a4
tyuukau <bachocnhi2002@gmail.com>
2023-05-22 09:18:50 +0700

    Frontend: Added userProfileUpdateReducer
    And registered it into the store

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -9,6 +9,10 @@ import {
   USER_PROFILE_REQUEST,
   USER_PROFILE_SUCCESS,
   USER_PROFILE_FAIL,
+  USER_PROFILE_UPDATE_REQUEST,
+  USER_PROFILE_UPDATE_SUCCESS,
+  USER_PROFILE_UPDATE_FAIL,
+  USER_PROFILE_UPDATE_RESET,
 } from "../constants/userConstants";
 
 /**
@@ -75,3 +79,18 @@ export const userProfileReducer = (state = { user: {} }, action) => {
       return state;
   }
 };
+
+export const userProfileUpdateReducer = (state = { }, action) => {
+  switch (action.type) {
+      case USER_PROFILE_UPDATE_REQUEST:
+        return { loading: true };
+      case USER_PROFILE_UPDATE_SUCCESS:
+        return { loading: false, success: true, userProfile: action.payload };
+      case USER_PROFILE_UPDATE_FAIL:
+        return { loading: false, error: action.payload };
+      case USER_PROFILE_UPDATE_RESET:
+        return { };
+      default:
+        return state;
+    }
+}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,21 +2,29 @@
 the `@reduxjs/toolkit` package. This function is used to create a Redux store with preconfigured
 options such as automatically combining slice reducers, adding middleware, and enabling the Redux
 DevTools Extension. */
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
 
 /* `import logger from 'redux-logger'` is importing the `redux-logger` middleware, which logs actions
 and state changes in the console for debugging purposes. It is then added to the middleware array in
 the `configureStore` function to be used in the Redux store. */
-import logger from 'redux-logger'
+import logger from "redux-logger";
 
 /* These lines of code are importing the reducer functions from separate files located in the
 `./reducers` directory. Specifically, `productListReducer` and `productDetailsReducer` are imported
 from `productReducers.js`, `cartReducer` is imported from `cartReducers.js`, and `userLoginReducer`
 is imported from `userReducers.js`. These reducer functions are then combined into a single
 `reducer` object that is passed to the `configureStore` function to create the Redux store. */
-import { productListReducer, productDetailsReducer } from './reducers/productReducers'
-import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer, userRegisterReducer, userProfileReducer } from './reducers/userReducers'
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userProfileReducer,
+  userProfileUpdateReducer,
+} from "./reducers/userReducers";
 
 /* `const reducer` is an object that combines all the individual reducer functions from separate files
 into a single object. Each reducer function is assigned to a key in the `reducer` object, with the
@@ -25,28 +33,31 @@ key name indicating the slice of state that the reducer function will manage. Fo
 `productList` slice of state in the Redux store. This `reducer` object is then passed to the
 `configureStore` function to create the Redux store. */
 const reducer = {
-    productList: productListReducer,
-    productDetails: productDetailsReducer,
-    cart: cartReducer,
-    userLogin: userLoginReducer,
-    userRegister: userRegisterReducer,
-    userProfile: userProfileReducer,
-}
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userProfile: userProfileReducer,
+  userProfileUpdate: userProfileUpdateReducer,
+};
 
 /* These lines of code are retrieving data from the browser's local storage and parsing it into
 the preloadedState object. */
-const cartItemsFromStorage = localStorage.getItem('cartItems') ?
-JSON.parse(localStorage.getItem('cartItems')) : []
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
 
-const userInfoFromStorage = localStorage.getItem('userInfo') ? 
-JSON.parse(localStorage.getItem('userInfo')) : null
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
 
 /* `const preloadedState` is an object that contains the initial state of the Redux store. It is used
 to hydrate the store with data that was previously saved in the browser's local storage. */
 const preloadedState = {
-    cart: { cartItems: cartItemsFromStorage },
-    userLogin: { userInfo: userInfoFromStorage }
-}
+  cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
+};
 
 // const debounceNotify = _.debounce((notify) => notify())
 
@@ -61,9 +72,9 @@ created Redux store, which is assigned to the `store` constant. */
 const store = configureStore({
   reducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
-  devTools: process.env.NODE_ENV !== 'production',
+  devTools: process.env.NODE_ENV !== "production",
   preloadedState,
-//   enhancers: [batchedSubscribe(debounceNotify)],
-})
+  //   enhancers: [batchedSubscribe(debounceNotify)],
+});
 
-export default store
+export default store;
