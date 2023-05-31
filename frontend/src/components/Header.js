a57c1912977f20661a886e179381606178a40215
tyuukau <bachocnhi2002@gmail.com>
2023-05-31 22:38:35 +0700

    FE: +admin buttons → Header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -63,6 +63,22 @@ function Header() {
                   </LinkContainer>
                 )}
 
+                {userInfo && userInfo.isAdmin && (
+                  <NavDropdown title="Admin" id="adminmenue">
+                    <LinkContainer to="/admin/userlist">
+                      <NavDropdown.Item>Users</NavDropdown.Item>
+                    </LinkContainer>
+
+                    <LinkContainer to="/admin/productlist">
+                      <NavDropdown.Item>Products</NavDropdown.Item>
+                    </LinkContainer>
+
+                    <LinkContainer to="/admin/orderlist">
+                      <NavDropdown.Item>Orders</NavDropdown.Item>
+                    </LinkContainer>
+                  </NavDropdown>
+                )}
+                
                 {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                   <NavDropdown.Item href="#action/3.2">
