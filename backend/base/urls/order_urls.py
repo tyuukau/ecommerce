d6bdc6343a9ee8f70bb8ab360e2e926f54d87eff
tyuukau <bachocnhi2002@gmail.com>
2023-06-01 12:34:29 +0700

    BE: +updateOrderToDelivered() → order_urls

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 
     path('<str:pk>/', views.getOrderById, name='user-order'),
     path('<str:pk>/pay/', views.updateOrderToPaid, name='pay'),
+    path('<str:pk>/deliver/', views.updateOrderToDelivered, name='order-delivered'),
     
     path('', views.getOrders, name='orders'),
     
