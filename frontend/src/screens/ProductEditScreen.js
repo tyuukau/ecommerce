8d6f834328385d6757d33c3673353d63b2357838
tyuukau <bachocnhi2002@gmail.com>
2023-06-01 12:15:14 +0700

    FE: Fix ProductEditScreen.js

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -19,7 +19,7 @@ function ProductEditScreen() {
   const [category, setCategory] = useState("");
   const [countInStock, setCountInStock] = useState(0);
   const [description, setDescription] = useState("");
-//   const [uploading, setUploading] = useState(false);
+  const [uploading, setUploading] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -70,34 +70,34 @@ function ProductEditScreen() {
     );
   };
 
-//   const uploadFileHandler = async (e) => {
-    // const file = e.target.files[0];
-    // const formData = new FormData();
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
 
-    // formData.append("image", file);
-    // formData.append("product_id", id);
+    formData.append("image", file);
+    formData.append("product_id", id);
 
-    // setUploading(true);
+    setUploading(true);
 
-    // try {
-    //   const config = {
-    //     headers: {
-    //       "Content-Type": "multipart/form-data",
-    //     },
-    //   };
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      };
 
-    //   const { data } = await axios.post(
-    //     "/api/products/upload/",
-    //     formData,
-    //     config
-    //   );
+      const { data } = await axios.post(
+        "/api/products/upload/",
+        formData,
+        config
+      );
 
-    //   setImage(data);
-    //   setUploading(false);
-    // } catch (error) {
-    //   setUploading(false);
-    // }
-//   };
+      setImage(data);
+      setUploading(false);
+    } catch (error) {
+      setUploading(false);
+    }
+  };
 
   return (
     <div>
@@ -143,13 +143,9 @@ function ProductEditScreen() {
                 onChange={(e) => setImage(e.target.value)}
               ></Form.Control>
 
-              {/* <Form.File
-                id="image-file"
-                label="Choose File"
-                custom
-                onChange={uploadFileHandler}
-              ></Form.File>
-              {uploading && <Loader />} */}
+              <Form.Control type="file" onChange={uploadFileHandler}/>
+              {uploading && <Loader />}
+
             </Form.Group>
 
             <Form.Group controlId="brand">
