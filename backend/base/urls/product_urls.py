3254de055e04c4d3e4ad9a277cc0636d7fab2c58
tyuukau <bachocnhi2002@gmail.com>
2023-06-01 11:11:05 +0700

    BE: +updateProduct() → product_urls

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.getProducts, name='products'),
     
     path('create/', views.createProduct, name="product-create"),
+    path('update/<str:pk>/', views.updateProduct, name="product-update"),
     path('delete/<str:pk>/', views.deleteProduct, name='product-delete'),
 
     path('<str:pk>/', views.getProduct, name='product'),
