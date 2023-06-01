c668070f6b5184a2e9cb7e11d4086ed719e88f52
tyuukau <bachocnhi2002@gmail.com>
2023-06-01 12:33:38 +0700

    BE: +updateOrderToDelivered() → order_views

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -114,3 +114,13 @@ def getOrders(request):
     serializer = OrderSerializer(orders, many=True)
     return Response(serializer.data)
  
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateOrderToDelivered(request, pk):
+    order = Order.objects.get(_id=pk)
+
+    order.isDelivered = True
+    order.deliveredAt = datetime.now()
+    order.save()
+
+    return Response('Order was delivered')
