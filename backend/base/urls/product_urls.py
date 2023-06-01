acfdc29176bac35b10f916949e124ed539259ce8
tyuukau <bachocnhi2002@gmail.com>
2023-06-01 10:20:44 +0700

    BE: +deleteProduct → product_urls

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -4,4 +4,6 @@ from ..views import product_views as views
 urlpatterns = [
     path('', views.getProducts, name='products'),
     path('<str:pk>/', views.getProduct, name='product'),
+    
+    path('delete/<str:pk>/', views.deleteProduct, name='product-delete')
 ]
